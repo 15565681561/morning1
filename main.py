29c9f306ff9641b114b299a650d923e14cd3f95b
15565681561 <74585605+15565681561@users.noreply.github.com>
2022-09-14 23:50:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def get_weather():
 
 # 获取情话
 def get_text():
-  url = "https://v2.alapi.cn/api/qinghua?token=LwExDtUWhF3rH5ib"
+  url = "https://api.vvhan.com/api/en?type=sj"
   res = requests.get(url).json()
   if res is None:
     return None
-  text = res['data']['content']
+  text = res['data']['zh']
   return text
 
 # 获取当前日期为星期几
@@ -79,10 +79,12 @@ def get_birthday_left():
 
 # 彩虹屁 接口不稳定，所以失败的话会重新调用，直到成功
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  url = "https://api.vvhan.com/api/en?type=sj"
+  res = requests.get(url).json()
+  if res is None:
+    return None
+  words = res['data']['en']
+  return words
 
 def format_temperature(temperature):
   return math.floor(temperature)
